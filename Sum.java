c15baf04f7fda3755b1a2e35c7a91231930c7fe8
Yash Mahajan <yashmahajan99@gmail.com>
2023-03-05 11:46:36 +0530

    modified commit

--- Sum.java
+++ Sum.java
@@ -2,8 +2,10 @@ public class Sum
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
+int n1 = 225, n2 = 115, sum,mult;  
 sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
+mult = n1*n2;
+System.out.println("The sum of numbers is: "+sum);
+System.out.println("The sum of numbers is: "+mult);  
 }  
 }  
